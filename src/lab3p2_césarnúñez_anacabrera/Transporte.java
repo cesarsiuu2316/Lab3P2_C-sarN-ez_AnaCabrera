0c1ff64ad3df34e3dce360b4ed4f8be0cf5a3602
César Núñez <cesar23augusto16@gmail.com>
2022-02-04 14:22:45 -0600

    Clase persona y transporte intento

--- src/lab3p2_césarnúñez_anacabrera/Transporte.java
+++ src/lab3p2_césarnúñez_anacabrera/Transporte.java
@@ -4,11 +4,56 @@ import java.util.ArrayList;
 
 public class Transporte {
     
-    protected int placa;
+    protected int placa; // única
     protected Color color;
     protected String transportista;
-    protected ArrayList<Estacion> estaciones = new ArrayList;
-    
+    protected ArrayList<Estacion> estaciones = new ArrayList();
+
+    public Transporte() {
+    }
+
+    public Transporte(int placa, Color color, String transportista) {
+        this.placa = placa;
+        this.color = color;
+        this.transportista = transportista;
+    }
+
+    public int getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(int placa) {
+        this.placa = placa;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public String getTransportista() {
+        return transportista;
+    }
+
+    public void setTransportista(String transportista) {
+        this.transportista = transportista;
+    }
+
+    public ArrayList<Estacion> getEstaciones() {
+        return estaciones;
+    }
+
+    public void setEstaciones(ArrayList<Estacion> estaciones) {
+        this.estaciones = estaciones;
+    }
+
+    @Override
+    public String toString() {
+        return "placa = " + placa + ", color = " + color + ", transportista = " + transportista + ", estaciones = " + estaciones;
+    }   
     
     
 } // clase
