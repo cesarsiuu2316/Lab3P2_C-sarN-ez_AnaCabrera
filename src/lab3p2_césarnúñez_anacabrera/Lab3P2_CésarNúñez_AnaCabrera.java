c8dc356ca3dd4dd068a17c42b53557d7f551e692
César Núñez <cesar23augusto16@gmail.com>
2022-02-04 14:55:08 -0600

    taxi y rapidito 2

--- src/lab3p2_césarnúñez_anacabrera/Lab3P2_CésarNúñez_AnaCabrera.java
+++ src/lab3p2_césarnúñez_anacabrera/Lab3P2_CésarNúñez_AnaCabrera.java
@@ -1,21 +1,9 @@
-
 package lab3p2_césarnúñez_anacabrera;
-
 import javax.swing.JOptionPane;
-
 
 public class Lab3P2_CésarNúñez_AnaCabrera {
 
-
     public static void main(String[] args) {
-       
-         String texto = "hola";
-        JOptionPane.showMessageDialog(null, texto + "Ana");
-        
-        int opcion = Integer.parseInt(JOptionPane.showInputDialog("1) \n"
-                + "2) \n"
-                + "3) "
-                + "Ingrese una opción"));
         
     }
     
