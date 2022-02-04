d1aa8c12d0a33a885b2357f4b53d63701029dea9
Bryan BAC Ana Contreras <BAC@MacBook-Air-de-Mac.local>
2022-02-04 15:11:02 -0600

    Main

--- src/lab3p2_césarnúñez_anacabrera/Lab3P2_CésarNúñez_AnaCabrera.java
+++ src/lab3p2_césarnúñez_anacabrera/Lab3P2_CésarNúñez_AnaCabrera.java
@@ -1,10 +1,22 @@
 package lab3p2_césarnúñez_anacabrera;
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class Lab3P2_CésarNúñez_AnaCabrera {
+    
+    public Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        
-    }
+ 
+
+}
     
+    public void menu(){
+        
+            int opcion = Integer.parseInt(JOptionPane.showInputDialog(" M E N U)\n"
+                    + "0. Salir \n"
+                    + "));
+                    
+        
+      }
 }
